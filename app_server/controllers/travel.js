4e3a28767941e52f0aac1e53423ad6a5c83c707a
Brian Zavala <brian.zavala25@proton.me>
2026-03-23 13:11:15 -0500

    Complete Module 5: Refactor MVC app to separate REST API and achieve Separation of Concerns

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,18 +1,53 @@
-const mongoose = require('mongoose');
-const Trip = mongoose.model('trips');
+const request = require('request');
+const apiOptions = {
+    server: 'http://localhost:3000'
+};
 
-const travel = async (req, res) => {
-  try {
-    const trips = await Trip.find({}).exec();
-    res.render("travel", {
-      title: "Travlr Getaways",
-      trips: trips,
+/* GET travel view */
+const travelList = (req, res) => {
+    const path = '/api/trips';
+    const requestOptions = {
+        url: `${apiOptions.server}${path}`,
+        method: 'GET',
+        json: {},
+    };
+
+    console.info('>> travelController.travelList calling ' + requestOptions.url);
+
+    request(
+        requestOptions,
+        (err, { statusCode }, body) => {
+            if (err) {
+                console.error(err);
+            }
+            _renderTravelList(req, res, body);
+        }
+    );
+};
+
+/* Internal method to render the travel list */
+const _renderTravelList = (req, res, responseBody) => {
+    let message = null;
+    let trips = [];
+
+    if (!(responseBody instanceof Array)) {
+        message = 'API lookup error';
+        trips = [];
+    } else {
+        if (!responseBody.length) {
+            message = 'No trips found in database';
+        } else {
+            trips = responseBody;
+        }
+    }
+
+    res.render('travel', {
+        title: 'Travlr Getaways',
+        trips: trips,
+        message: message
     });
-  } catch (err) {
-    res.status(500).send(err);
-  }
 };
 
 module.exports = {
-  travel,
+    travelList
 };
